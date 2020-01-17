214b900dc383566e0457776b1b5ae29202bfbe15
Camilo <Lestani>
2020-01-17 09:52:29 -0300

    Merge branch 'integrationCrud' of C:\Users\clestani\Desktop\APIlibreriabau\APILibreriaBAU with conflicts.

--- src/main/java/com/baufest/Libreria/repository/ClienteRepository.java
+++ src/main/java/com/baufest/Libreria/repository/ClienteRepository.java
@@ -2,6 +2,10 @@ package com.baufest.Libreria.repository;
 
 import com.baufest.Libreria.models.Cliente;
 
+<<<<<<< HEAD
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+=======
+public interface ClienteRepository extends RepositoryCustom<Cliente,Integer> {
+>>>>>>> integrationCrud
 }
